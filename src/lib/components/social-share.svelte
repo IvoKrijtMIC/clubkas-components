143426ea3492c9264213d5c73adef67d6e8a48fe
Ivanho101 <ivo@mobileinteractioncompany.com>
2023-09-05 10:59:55 +0200

    Change social share to resemble social slide

--- src/lib/components/social-share.svelte
+++ src/lib/components/social-share.svelte
@@ -2,6 +2,14 @@
 	export let text: string;
 	export let link: string;
 	export let title: string;
+	export let showExtra = true;
+	export let showFb = true;
+	export let showLi = true;
+	export let showX = true;
+	export let showMail = true;
+	export let showCopy = true;
+	export let showQR = false;
+
 	function copyLink() {
 		navigator.clipboard.writeText(link);
 	}
@@ -18,51 +26,85 @@
 			<span>Deel via WhatsApp</span>
 		</a>
 	</div>
-	<div
-		class="extra-buttons relative flex justify-center gap-2 my-2 z-0 transition-all ease-in-out duration-1000"
-		id="extra-buttons"
-	>
-		<a class="fb-share" href={"https://www.facebook.com/sharer/sharer.php?u=" + link} target="_blank">
-			<img
-				class="h-auto w-10"
-				src="https://mic-production-upload-image-share.s3.eu-central-1.amazonaws.com/M.I.C/Collecte+365+V2/Facebook+share.png"
-				alt="Via Facebook delen"
-			/>
-		</a>
-		<a class="li-share" href={"https://www.linkedin.com/shareArticle?mini=true&url=" + link + "&title=" + title} target="_blank">
-			<img
-				class="h-auto w-10"
-				src="https://mic-production-upload-image-share.s3.eu-central-1.amazonaws.com/M.I.C/Collecte+365+V2/LinkedIn+share.png"
-				alt="Via LinkedIn delen"
-			/>
-		</a>
-		<a class="x-share" href={"https://twitter.com/intent/tweet?text=" + text + link} target="_blank">
-			<img
-				class="h-auto w-10"
-				src="https://mic-production-upload-image-share.s3.eu-central-1.amazonaws.com/M.I.C/x-logo-white.png"
-				alt="Via E-mail delen"
-			/>
-		</a>
-		<a class="mail-share" href={"mailto:?subject=" + title + "&body=" + text + " " + link} target="_blank">
-			<img
-				class="h-auto w-10"
-				src="https://mic-production-upload-image-share.s3.eu-central-1.amazonaws.com/M.I.C/Collecte+365+V2/E-mail+share.png"
-				alt="Via E-mail delen"
-			/>
-		</a>
-		<button data-tip="Deellink gekopieerd" class="copyurl-share cursor-pointer" on:click={copyLink}>
-			<img
-				class="h-auto w-10"
-				src="https://mic-production-upload-image-share.s3.eu-central-1.amazonaws.com/M.I.C/Collecte+365+V2/Link+share.png"
-				alt="Kopieer je deellink"
-			/>
-		</button>
-		<a class="qr-share" href="">
-			<img
-				class="h-auto w-10"
-				src="https://mic-production-upload-image-share.s3.eu-central-1.amazonaws.com/Hartstichting/QR.png"
-				alt="Via E-mail delen"
-			/>
-		</a>
-	</div>
+	{#if showExtra}
+		<div
+			class="show-others relative flex justify-center gap-2 my-2 mr-11 z-0 opacity-100 hide"
+			id="show-others"
+		>
+			{#if showFb}
+				<a
+					class="fb-share block"
+					href={'https://www.facebook.com/sharer/sharer.php?u=' + link}
+					target="_blank"
+				>
+					<img
+						class="h-auto w-10"
+						src="https://mic-production-upload-image-share.s3.eu-central-1.amazonaws.com/M.I.C/Collecte+365+V2/Facebook+share.png"
+						alt="Via Facebook delen"
+					/>
+				</a>
+			{/if}
+			{#if showLi}
+				<a
+					class="li-share"
+					href={'https://www.linkedin.com/shareArticle?mini=true&url=' + link + '&title=' + title}
+					target="_blank"
+				>
+					<img
+						class="h-auto w-10"
+						src="https://mic-production-upload-image-share.s3.eu-central-1.amazonaws.com/M.I.C/Collecte+365+V2/LinkedIn+share.png"
+						alt="Via LinkedIn delen"
+					/>
+				</a>
+			{/if}
+			{#if showX}
+				<a
+					class="x-share"
+					href={'https://twitter.com/intent/tweet?text=' + text + link}
+					target="_blank"
+				>
+					<img
+						class="h-auto w-10"
+						src="https://mic-production-upload-image-share.s3.eu-central-1.amazonaws.com/M.I.C/x-logo-white.png"
+						alt="Via E-mail delen"
+					/>
+				</a>
+			{/if}
+			{#if showMail}
+				<a
+					class="mail-share"
+					href={'mailto:?subject=' + title + '&body=' + text + ' ' + link}
+					target="_blank"
+				>
+					<img
+						class="h-auto w-10"
+						src="https://mic-production-upload-image-share.s3.eu-central-1.amazonaws.com/M.I.C/Collecte+365+V2/E-mail+share.png"
+						alt="Via E-mail delen"
+					/>
+				</a>
+			{/if}
+			{#if showCopy}
+				<button
+					data-tip="Deellink gekopieerd"
+					class="copyurl-share cursor-pointer"
+					on:click={copyLink}
+				>
+					<img
+						class="h-auto w-10"
+						src="https://mic-production-upload-image-share.s3.eu-central-1.amazonaws.com/M.I.C/Collecte+365+V2/Link+share.png"
+						alt="Kopieer je deellink"
+					/>
+				</button>
+			{/if}
+			{#if showQR}
+				<a class="qr-share" href="">
+					<img
+						class="h-auto w-10"
+						src="https://mic-production-upload-image-share.s3.eu-central-1.amazonaws.com/Hartstichting/QR.png"
+						alt="Via E-mail delen"
+					/>
+				</a>
+			{/if}
+		</div>
+	{/if}
 </div>
