0fdbdd5f37d696c0594890544ae67563dd964ac7
Ivanho101 <ivo@mobileinteractioncompany.com>
2023-09-11 16:40:23 +0200

    Remove simple components, use layout from svelte, relocate app.css, rename app.css to index.css, change styling

--- src/lib/components/footer-card.svelte
+++
@@ -1,8 +0,0 @@
-<div class="footer-card flex justify-between p-5">
-	<div class="footer-item flex flex-col gap-2.5">
-        <slot />
-	</div>
-	<div class="footer-item self-end">
-		<a href="https://www.mobileinteractioncompany.com/"><img alt="" src="https://mic-production-upload-image-share.s3.eu-central-1.amazonaws.com/M.I.C/Powered_by_MIC.png" class="w-20 min-w-[5rem]" /></a>
-	</div>
-</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,18 @@ export default {
         'dark-gray-mic': '#e5e5e5',
         'blue-mic': '#1d3dff',
       },
+      boxShadow: {
+        'mic': '0 0 4px 2px rgba(0, 0, 0, .1)',
+      },
+      outlineColor: {
+        'mic': '#f55',
+      },
+      outlineWidth: {
+        'mic': '2px',
+      },
+      outlineOffset: {
+        'mic': '0',
+      },
     },
   },
   plugins: [],
